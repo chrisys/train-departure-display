aa15e3d71c697e54feae88e572c2ee18e0aab5da
Jonathan Silver <55157939+jajsilver@users.noreply.github.com>
2019-10-14 09:18:24 +0100

    new feature: timeOffset parameter
    Environment variable "timeOffset" can be added to change the time window that the departure board displays - see readme

--- balena-run.sh
+++ balena-run.sh
@@ -4,9 +4,9 @@ if [ ! -f config.json ]; then
   cp config.sample.json config.json
   jq .journey.departureStation=\""${departureStation}"\" config.json | sponge config.json
   jq .journey.destinationStation=\""${destinationStation}"\" config.json | sponge config.json
+  jq .journey.timeOffset=\""${timeOffset}"\" config.json | sponge config.json
   jq .journey.outOfHoursName=\""${outOfHoursName}"\" config.json | sponge config.json
   jq .refreshTime="${refreshTime}" config.json | sponge config.json
-  jq .transportApi.appId=\""${transportApi_appId}"\" config.json | sponge config.json
   jq .transportApi.apiKey=\""${transportApi_apiKey}"\" config.json | sponge config.json
   jq .transportApi.operatingHours=\""${transportApi_operatingHours}"\" config.json | sponge config.json
 fi
