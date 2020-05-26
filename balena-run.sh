5201123f00036bb1ad846f3a2fe32c52f64deaed
Chris Crocker-White <chriscw@balena.io>
2020-05-26 13:50:34 +0100

    Improve local development
    Add a config.json which is used for default variables (meaning they are not required to be specified on commandline).
    Make balena-run.sh override config.json with any environment variables that *are* set
    Add use of PiWheels making local builds on Pi Zero much much faster (a few minutes for a clean build)
    
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- balena-run.sh
+++ balena-run.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-if [ ! -f config.json ]; then
-  cp config.sample.json config.json
+if [[ ! -z "${departureStation}" ]]; then
   jq .journey.departureStation=\""${departureStation}"\" config.json | sponge config.json
+fi
+
+if [[ ! -z "${destinationStation}" ]]; then
   jq .journey.destinationStation=\""${destinationStation}"\" config.json | sponge config.json
+fi
+
+if [[ ! -z "${outOfHoursName}" ]]; then
   jq .journey.outOfHoursName=\""${outOfHoursName}"\" config.json | sponge config.json
+fi
+
+if [[ ! -z "${refreshTime}" ]]; then
   jq .refreshTime="${refreshTime}" config.json | sponge config.json
+fi
+
+if [[ ! -z "${transportApi_appId}" ]]; then
   jq .transportApi.appId=\""${transportApi_appId}"\" config.json | sponge config.json
+fi
+
+if [[ ! -z "${transportApi_apiKey}" ]]; then
   jq .transportApi.apiKey=\""${transportApi_apiKey}"\" config.json | sponge config.json
+fi
+
+if [[ ! -z "${transportApi_operatingHours}" ]]; then
   jq .transportApi.operatingHours=\""${transportApi_operatingHours}"\" config.json | sponge config.json
 fi
 
