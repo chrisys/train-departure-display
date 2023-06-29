44f9e4639d40a335004bc97a870819fd8488f358
Paul Curry <cr3ative@users.noreply.github.com>
2023-06-29 13:40:03 +0100

    Performance branch (#1)
    # Performance Branch
    
    The most computationally expensive thing to do on any computer is render a font. It's what Babbage fought with for years.
    
    I profiled the main loop of this code by vendoring in Luma, removing the threadpool, and running cProfile over it. That revealed two things:
    
    * Running the seconds as "hotspot" was burning up the CPU
    * The rest of the CPU time was spent "scrolling" the "calling at stations" as it required a full re-render of the string each time a character was dropped off the front of the string.
    * We've changed the scrolling behaviour to scroll the bitmap at 1 pixel per frame, rather than 1 character per frame. Smooth!
      * There's also a snazzy little rising-up animation for fun.
    * The other frequent font calls (in the loop) have all been bitmap cached too.
    * The frame regulator is now configurable; it fights with the CPU on a Pi Zero and is better disabled there; but on a Pi3 you want a regulator to stop burning the CPU up unnecessarily.
    
    So, main fixes were to put the seconds as an interval-updated zone with 0.1 second resolution, and to pre-render all other commonly used TTF operations in the main loop.
    
    ## Results
    
    On a Pi Zero (the oldest possible device!), here's the performance on "main":
    
    ![353895428_652705433582787_8305305711463697195_n](https://github.com/CalamityJames/train-departure-display/assets/1850718/823cfcc8-1f6b-4730-ae5d-f49e655af10f)
    
    And here's the performance on "performance", with `targetFPS` set to `0`:
    
    ![image](https://github.com/CalamityJames/train-departure-display/assets/1850718/c1a260a1-cd26-4872-b204-4654293caa9a)
    
    # Changelog
    
    See updated CHANGELOG
    
    ---------
    
    Co-authored-by: James <me@calamityjames.dev>

--- src/config.py
+++ src/config.py
@@ -1,24 +1,33 @@
 import os
 import re
 
+
 def loadConfig():
     data = {
         "journey": {},
         "api": {}
     }
 
-    data["refreshTime"] = int(os.getenv("refreshTime") or 30)
+    data["targetFPS"] = int(os.getenv("targetFPS") or 70)
+    data["refreshTime"] = int(os.getenv("refreshTime") or 180)
+    data["fpsTime"] = int(os.getenv("fpsTime") or 180)
     data["screenRotation"] = int(os.getenv("screenRotation") or 2)
     data["screenBlankHours"] = os.getenv("screenBlankHours") or ""
+    data["headless"] = False
+    if os.getenv("headless") == "True":
+        data["headless"] = True
     data["emulator"] = False
     if os.getenv("emulator") == "True":
-        data["emulator"] = True    
+        data["emulator"] = True
     data["dualScreen"] = False
     if os.getenv("dualScreen") == "True":
         data["dualScreen"] = True
+    data["firstDepartureBold"] = True
+    if os.getenv("firstDepartureBold") == "False":
+        data["firstDepartureBold"] = False
     data["hoursPattern"] = re.compile("^((2[0-3]|[0-1]?[0-9])-(2[0-3]|[0-1]?[0-9]))$")
 
-    data["journey"]["departureStation"] = os.getenv("departureStation") or "STP"
+    data["journey"]["departureStation"] = os.getenv("departureStation") or "PAD"
 
     data["journey"]["destinationStation"] = os.getenv("destinationStation") or ""
     if data["journey"]["destinationStation"] == "null" or data["journey"]["destinationStation"] == "undefined":
@@ -29,15 +38,15 @@ def loadConfig():
         data["journey"]["individualStationDepartureTime"] = True
 
     data["journey"]["outOfHoursName"] = os.getenv("outOfHoursName") or "London Paddington"
-    data["journey"]["stationAbbr"] = { "International": "Intl." }
+    data["journey"]["stationAbbr"] = {"International": "Intl."}
     data["journey"]['timeOffset'] = os.getenv("timeOffset") or "0"
-    data["journey"]["screen1Platform"] = os.getenv("screen1Platform")
-    data["journey"]["screen2Platform"] = os.getenv("screen2Platform")
+    data["journey"]["screen1Platform"] = os.getenv("screen1Platform") or ""
+    data["journey"]["screen2Platform"] = os.getenv("screen2Platform") or ""
 
-    if data["journey"]["screen1Platform"].isnumeric() != True:
+    if data["journey"]["screen1Platform"].isnumeric() is not True:
         data["journey"]["screen1Platform"] = ""
 
-    if data["journey"]["screen2Platform"].isnumeric() != True:
+    if data["journey"]["screen2Platform"].isnumeric() is not True:
         data["journey"]["screen2Platform"] = ""
 
     data["api"]["apiKey"] = os.getenv("apiKey") or None
@@ -48,4 +57,3 @@ def loadConfig():
         data["showDepartureNumbers"] = True
 
     return data
-    
--- src/open.py
+++ src/open.py
@@ -1,12 +1,14 @@
 from datetime import datetime, time
 
+
 def is_time_between(begin_time, end_time, check_time=None):
     # If check time is not given, default to current UTC time
     check_time = check_time or datetime.now().time()
     if begin_time < end_time:
         return check_time >= begin_time and check_time <= end_time
-    else: # crosses midnight
+    else:  # crosses midnight
         return check_time >= begin_time or check_time <= end_time
 
+
 def isRun(start_hour, end_hour):
-    return is_time_between(time(start_hour,0), time(end_hour,0))
+    return is_time_between(time(start_hour, 0), time(end_hour, 0))
